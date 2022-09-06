abc6f34a6cd1a14b3d4686f1a548a49c12cccacb
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-09-06 12:30:12 +0300

    commit tree 4be1e1035d286879962d9c2deb5d9dd258eec7a9

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+   "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    names = []
+    self.characters.each do |char|
+        self.shows.each do |show|
+            names << "#{char.name} - #{show.name}"
+        end
+    end
+    names
+  end
+
+
 end
